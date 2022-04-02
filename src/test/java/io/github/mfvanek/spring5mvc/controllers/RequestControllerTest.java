2d2673c5c4f08012cd659efce20f2a64b235668b
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2022-04-02 22:39:46 +0300

    Updated dependencies (#5)
    * Updated dependencies
    
    * Added badge
    
    * Added test for swagger
    
    * Removed hamcrest; migrated to AssertJ
    
    Co-authored-by: Ivan Vakhrushev <i.vakhrushev@banki.ru>

--- src/test/java/io/github/mfvanek/spring5mvc/controllers/RequestControllerTest.java
+++ src/test/java/io/github/mfvanek/spring5mvc/controllers/RequestControllerTest.java
@@ -1,13 +1,13 @@
 package io.github.mfvanek.spring5mvc.controllers;
 
 import io.github.mfvanek.spring5mvc.config.WebConfig;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -16,71 +16,79 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.ServletContext;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.startsWith;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {WebConfig.class})
 @WebAppConfiguration
-public class RequestControllerTest {
+class RequestControllerTest {
 
     @Autowired
     private WebApplicationContext wac;
-
     private MockMvc mockMvc;
 
-    @Before
+    @BeforeEach
     public void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
     @Test
-    public void verifyTestConfiguration() {
+    void verifyTestConfiguration() {
         ServletContext servletContext = wac.getServletContext();
 
-        assertNotNull(servletContext);
-        assertThat(servletContext, instanceOf(MockServletContext.class));
-        assertNotNull(wac.getBean("requestController"));
+        assertThat(servletContext).isNotNull();
+        assertThat(servletContext).isInstanceOf(MockServletContext.class);
+        assertThat(wac.getBean("requestController"))
+                .isNotNull()
+                .isInstanceOf(RequestController.class);
     }
 
     @Test
-    public void log() throws Exception {
+    void apiDocsShouldWork() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(get("/v2/api-docs"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertEquals("application/json", mvcResult.getResponse().getContentType());
+        assertThat(mvcResult.getResponse().getContentAsString()).startsWith("{\"swagger\":\"2.0\"");
+    }
+
+    @Test
+    void log() throws Exception {
         MvcResult mvcResult = this.mockMvc.perform(get("/api/v1/demo"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
 
         assertEquals("text/plain;charset=ISO-8859-1", mvcResult.getResponse().getContentType());
-        assertThat(mvcResult.getResponse().getContentAsString(), startsWith("Add log entry at "));
+        assertThat(mvcResult.getResponse().getContentAsString()).startsWith("Add log entry at ");
     }
 
     @Test
-    public void logException() throws Exception {
+    void logException() throws Exception {
         MvcResult mvcResult = this.mockMvc.perform(get("/api/v1/demoException"))
                 .andDo(print())
                 .andExpect(status().is5xxServerError())
-                .andExpect(jsonPath("$.message").value("sample exception"))
                 .andReturn();
 
         assertEquals("application/json", mvcResult.getResponse().getContentType());
+        assertThat(mvcResult.getResponse().getContentAsString()).contains("\"message\":\"sample exception\"");
     }
 
     @Test
-    public void displayWelcomeMessage() throws Exception {
+    void displayWelcomeMessage() throws Exception {
         MvcResult mvcResult = this.mockMvc.perform(get("/api/v1/welcome"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title").value("Jaeger tracing demo"))
-                .andExpect(jsonPath("description").value(startsWith("Welcome from Spring MVC and Embedded Jetty. ")))
                 .andReturn();
 
         assertEquals("application/json", mvcResult.getResponse().getContentType());
+        assertThat(mvcResult.getResponse().getContentAsString())
+                .startsWith("{\"title\":\"Jaeger tracing demo\",\"description\":\"Welcome from Spring MVC and Embedded Jetty. ");
     }
 }
